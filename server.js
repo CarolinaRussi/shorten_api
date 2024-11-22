9099d815bbdceabf329e177bdf7960e7c5b1434b
Carolina Russi <carolinaferlarussi@gmail.com>
2024-11-22 09:05:46 -0300

    Add dynamic port

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors()); 
-
+app.use(cors());
 app.use("/", urlRoutes);
 
-app.listen(8800);
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 8800;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
+
+export default app;
